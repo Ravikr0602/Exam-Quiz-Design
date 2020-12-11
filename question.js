211fa6315236f35865271e058d82c31f0af2a180
Ravi Kumar <ravikumar060296@gmail.com>
2020-12-11 19:03:08 +0530

    Exam Quiz design using Html, Css, JavaScript

---
+++ question.js
@@ -0,0 +1,35 @@
+//Array of Object
+const quiz = [
+    {
+        q:'Correct HTML tag for the Largest Heading Is?',
+        options:['h6','heading','head','h1'],
+        answer:3
+    },
+    {
+        q:'Which of the following property is used to control the position of an image in the background?',
+        options:['background-color','background-image','background-repeat','background-position'],
+        answer:3
+    },
+    {
+        q:'In SQL Command To search for a specified pattern in a column',
+        options:['SQL MATCH','SQL LIKE','SQL PATTERN','SQL SEARCH'],
+        answer:1
+    },
+    {
+        q:'Which of the following is not a Java features?',
+        options:['Dynamic','Architecture Neutral','Use of pointers','Object-oriented'],
+        answer:2
+    },
+    {
+        q:'XML is a markup language where documents must be marked up correctly?',
+        options:['True','False'],
+        answer:0
+    }
+    ,
+    {
+        q:'what is 2 + 6 ?',
+        options:['13','7','9','8'],
+        answer:3
+    }
+    
+]
